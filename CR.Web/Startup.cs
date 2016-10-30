83a840a6c12dda9b05c89c45821da7b2a0d5d051
ChrisLaptop <ChrisLaptop@CHRISLAPTOP>
2016-10-30 09:59:29 -0500

    Agregar archivos de proyecto.

---
+++ CR.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CR.Web.Startup))]
+namespace CR.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
